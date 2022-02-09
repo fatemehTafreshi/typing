830027b04235717f13a78c327d0487a588600bb3
fatemeh tafreshi <tafreshifatemeh12@gmail.com>
2022-02-09 10:42:38 +0330

    checking input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import faker from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'typing';
+  RandomText:string=faker.lorem.sentence();
+  InputText:string='';
+
+  onChangeInput(text:any) {
+    this.InputText=text.target.value;
+  }
+
+  onCompare(item: string, inputTextElement: string):string {
+    if(!inputTextElement) {
+      return 'pending';
+    }
+    return item === inputTextElement ? 'equal' : 'notEqual';
+
+  }
 }
